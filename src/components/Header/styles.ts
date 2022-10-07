353080bfec9cf930a29c80cd3b95c285fa06f9ea
Tiago Castro <tiagocastro602@gmail.com>
2022-10-06 22:52:57 -0300

    default layout

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 
 export const Container = styled.header`
   width: 100%;
-  max-width: 1120px;
   padding: 32px 0;
   margin: 0 auto;
   display: flex;
--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,10 +1,6 @@
-import { Header } from '../../components/Header'
-
 export function Checkout() {
   return (
     <>
-      <Header />
-
       <h1>Checkout</h1>
     </>
   )
